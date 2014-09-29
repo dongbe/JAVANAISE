6b26f6d7cfab341dfd64345a20d35ceb4765e8de
Don <donatien@Ubuntu-Don>
2014-09-29 09:40:03 +0200

    coordImpl

---
+++ SOURCES/src/jvn/JvnStatus.java
@@ -0,0 +1,36 @@
+package jvn;
+
+import java.io.Serializable;
+
+import irc.Sentence.State;
+
+public class JvnStatus {
+	
+	private JvnRemoteServer js;
+	private Serializable state;
+	
+	public JvnRemoteServer getJs() {
+		return js;
+	}
+
+	public void setJs(JvnRemoteServer js) {
+		this.js = js;
+	}
+
+	public Serializable getState() {
+		return state;
+	}
+
+	public void setState(Serializable state) {
+		this.state = state;
+	}
+
+	public JvnStatus(JvnRemoteServer js, Serializable st){
+		this.js=js;
+		this.state=st;
+	}
+
+
+	
+
+}
